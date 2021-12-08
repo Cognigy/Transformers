98d2cdc87674c85b7c4c2f5a0a2f2d98f72de58d
AlexTeusz <mail@alexteusz.de>
2021-12-08 10:14:32 +0100

    limit quick replies to three for output

--- endpoint/webhook/tyntec-whatsapp/transformer.ts
+++ endpoint/webhook/tyntec-whatsapp/transformer.ts
@@ -16,7 +16,7 @@ const TYNTEC_API_KEY = ""; // Tyntec API Key
 
 //session timeout in seconds, new session gets generated afterwards
 //disable by setting to 0
-const SESSION_TIMEOUT = 30;
+const SESSION_TIMEOUT = 1800;
 
 const HIDE_USER_ID = true
 const HIDE_SESSION_ID = true
@@ -160,7 +160,7 @@ const createWhatsAppQuickReplies = (quickReplies: IDefaultQuickReply[]): IWhatsA
 		});
 	}
 
-	return whatsAppQuickReplies;
+	return whatsAppQuickReplies.slice(0,3);
 }
 
 
@@ -313,18 +313,6 @@ const convertWebchatContentToWhatsApp = (output, sessionId: string, sessionStora
 }
 
 createWebhookTransformer({
-
-	/**
-	 * This transformer is executed when receiving a message
-	 * from the user, before executing the Flow.
-	 *
-	 * @param endpoint The configuration object for the used Endpoint.
-	 * @param request The Express request object with a JSON parsed body.
-	 * @param response The Express response object.
-	 *
-	 * @returns A valid userId, sessionId, as well as text and/or data,
-	 * which has been extracted from the request body.
-	 */
 	handleInput: async ({ endpoint, request, response }) => {
 
 		// handle accepted Tyntec WhatsApp messages
@@ -391,25 +379,6 @@ createWebhookTransformer({
 			data
 		};
 	},
-
-	/**
-	 * This transformer is executed on every output from the Flow.
-	 * For Webhook based transformers, the return value of this transformer
-	 * will be sent directly to the user.
-	 *
-	 * @param processedOutput The output from the Flow that has been processed into the final object
-	 * that will be sent to the user. It is structured according to the data structure used
-	 * on the specific Endpoint channel.
-	 *
-	 * @param output The raw output from the Flow.
-	 * @param endpoint The configuration object for the used Endpoint.
-	 * @param userId The unique ID of the user.
-	 * @param sessionId The unique ID for this session. Can be used together with the userId
-	 * to retrieve the sessionStorage object.
-	 *
-	 * @returns An object that will be sent to the user, unchanged. It therefore has to have the
-	 * correct format according to the documentation of the specific Endpoint channel.
-	 */
 	handleOutput: async ({ processedOutput, output, endpoint, userId, sessionId }) => {
 		//create output transformer translation storage
 		const processedSessionStorage = await getSessionStorage(userId, sessionId);
@@ -425,13 +394,12 @@ createWebhookTransformer({
 		let whatsapp: TWhatsAppContent[] = convertWebchatContentToWhatsApp(output, clearSessionId, processedSessionStorage);
 		if (!whatsapp.length) {
 			console.error("Missing WhatsApp compatible channel output!");
-			console.log(JSON.stringify(output))
 			return
 		}
-		let result;
-		// decide whether to use the bulks or messages API. If there is only one message, use the messages API.
+
+		// Decide whether to use the bulks or messages API. If there is only one message, use the messages API.
 		if (whatsapp.length === 1) {
-			result = await httpRequest({
+			await httpRequest({
 				uri: "https://api.tyntec.com/chat-api/v2/messages",
 				method: "POST",
 				headers: {
@@ -449,7 +417,7 @@ createWebhookTransformer({
 				json: true
 			});
 		} else {
-			result = await httpRequest({
+			await httpRequest({
 				uri: "https://api.tyntec.com/chat-api/v2/bulks",
 				method: "POST",
 				headers: {
@@ -466,41 +434,10 @@ createWebhookTransformer({
 				json: true
 			});
 		}
-		console.log(result);
 		return null;
-
-		return processedOutput;
 	},
-
-	/**
-		 * This transformer is executed when the Flow execution has finished.
-	 * Since all outputs have been sent to the user, this transformer does not return anything.
-	 *
-	 * @param userId The unique ID of the user.
-	 * @param sessionId The unique ID for this session. Can be used together with the userId
-	 * to retrieve the sessionStorage object.
-	 *
-	 * @param endpoint The configuration object for the used Endpoint.
-	 *
-	 * @returns This transformer cannot return anything.
-	 */
 	handleExecutionFinished: async ({ sessionId, userId, endpoint }) => {
-
 	},
-
-	/**
-	 * This transformer is executed when receiving an inject event.
-	 * The extracted text and data will be injected into the conversation
-	 * for the given user in the given session.
-	 *
-	 * @param request The Express request object with a JSON parsed body.
-	 * @param response The Express response object.
-	 * @param endpoint The configuration object for the used Endpoint.
-	 *
-	 * @returns A valid userId, sessionId, as well as text and/or data,
-	 * which has been extracted from the request body. The text and data
-	 * will be injected into this conversation.
-	 */
 	handleInject: async ({ request, response, endpoint }) => {
 
 		/**
@@ -527,20 +464,6 @@ createWebhookTransformer({
 			data
 		};
 	},
-
-	/**
-	 * This transformer is executed when receiving a notify event.
-	 * The extracted text and data will be sent directly to the
-	 * given user in the given session as a notification.
-	 *
-	 * @param request The Express request object with a JSON parsed body.
-	 * @param response The Express response object.
-	 * @param endpoint The configuration object for the used Endpoint.
-	 *
-	 * @returns A valid userId, sessionId, as well as text and/or data,
-	 * which has been extracted from the request body. The text and data
-	 * will be sent directly to the user.
-	 */
 	handleNotify: async ({ request, response, endpoint }) => {
 
 		/**
