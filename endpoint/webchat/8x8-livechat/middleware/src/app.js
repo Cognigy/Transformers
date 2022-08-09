9f7faf180333588cd60a8576f0a1ecc9de1423e0
Alex Teusz <a.teusz@cognigy.com>
2022-08-09 14:28:50 +0200

    initial middleware

--- endpoint/webchat/8x8-livechat/middleware/src/app.js
+++ endpoint/webchat/8x8-livechat/middleware/src/app.js
@@ -7,27 +7,63 @@ const port = 8181;
 // Enable JSON Body
 app.use(express.json({ type: () => true }));
 
-app.post('/inject', async (req, res) => {
+app.post('/inject/:region/tenant/:tenantId', async (req, res) => {
 
-    const { body } = req;
+    const { body, params } = req;
+    const { region, tenantId } = params;
+
+    // Check if the Webhook must be validated by the 8x8 platform for configuration
+    // Sent body: {"notificationVersion":"v2.0","eventType":"WEB_HOOK_VERIFY"}
+    if (req?.body?.eventType === 'WEB_HOOK_VERIFY') {
+        res.sendStatus(200);
+    }
 
     try {
-        await axios({
-            method: "post",
-            url: `${cogEndpointUrl}/inject/${URLToken}`,
-            data: {
-                userId,
-                text: "",
-                data: body,
-                sessionId
+
+        // Get the conversation details
+        const conversationDetailsResponse = await axios({
+            method: 'get',
+            url: `https://api.8x8.com/vcc/${region}/chat/v2/tenant/${tenantId}/conversations/${body?.conversationId}`,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer zZSAmQIn5AYmJ7GvuXRb4Rr2Xvo3`
             }
         });
+
+        const { customer } = conversationDetailsResponse?.data;
+        const { cognigySessionId, cognigyURLToken, cognigyUserId } = customer;
+
+        // Check if the agent sent a message or a different event happened
+        if (body?.eventType === 'TEXT') {
+            await axios({
+                method: 'post',
+                url: `https://endpoint-trial.cognigy.ai/notify/${cognigyURLToken}`,
+                data: {
+                    userId: cognigyUserId,
+                    text: body?.message,
+                    data: null,
+                    sessionId: cognigySessionId
+                }
+            });
+        } else {
+            await axios({
+                method: 'post',
+                url: `https://endpoint-trial.cognigy.ai/inject/${cognigyURLToken}`,
+                data: {
+                    userId: cognigyUserId,
+                    text: '',
+                    data: body,
+                    sessionId: cognigySessionId
+                }
+            });
+        }
 
         if (!res.headersSent) {
             res.status(200).send("");
         }
     } catch (error) {
-        res.send(error)
+        res.send(error);
+        console.log(error.message)
     }
 });
 
